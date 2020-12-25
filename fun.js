5ed13b31f31c392a9c2a89d21af25fae0182d556
khaedzi <khaled.ziadeh@yahoo.com>
2020-12-25 16:35:16 +0200

    othing i edited

---
+++ fun.js
@@ -0,0 +1,18 @@
+var fnum = prompt("give me first number please :");
+var snum=prompt("give me secound number please :");
+var dis;
+function colaction(){
+if(dis="+"){
+
+    sum();
+}
+
+
+}
+
+function sum(){
+
+sum1=parseInt(fnum)+parseInt(snum);
+alert(sum1);
+
+}
